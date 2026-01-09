757b36fd0bab34265000584686acf694869d61d2
DELL <DELL@DESKTOP-NELOF1J>
2026-01-09 15:42:12 +0700

    Add project files.

---
+++ Restaurant/Program.cs
@@ -0,0 +1,21 @@
+namespace Restaurant;
+
+internal static class Program
+{
+    /// <summary>
+    ///  By: Pacharadanai Kurakanog ID. 145
+    /// </summary>
+    [STAThread]
+    static void Main(string[] args)
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        // Application.Run(new KitchenForm());
+
+        if (args.Length > 0 && args[0].ToLower() == "-k")
+            Application.Run(new KitchenForm());
+        else
+            Application.Run(new WaiterForm());
+    }
+}
