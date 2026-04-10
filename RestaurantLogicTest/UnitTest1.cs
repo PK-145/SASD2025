60404bac89384194bdc1d9c526e79eb66fddb57d
DELL <DELL@DESKTOP-CT9ESO3>
2026-04-10 15:06:32 +0700

    Init LogicTest

---
+++ RestaurantLogicTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RestaurantLogicTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
