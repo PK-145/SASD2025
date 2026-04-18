ead12e63764a4a8181a66a4e62d379d154ff4f1b
ScythK0TH <scythk0th5@gmail.com>
2026-04-18 11:42:34 +0700

    Add WinForm1 project with login and display screens
    Added new WinForm1 Windows Forms project targeting .NET 8.0. Includes main form with screen switching, LoginScreen for user entry, and DisplayScreen for viewing sample Person data. Updated solution file to include the new project and build configs. Application now starts with login and transitions to data display after login.

---
+++ WinForm1/DisplayScreen.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WinForm1
+{
+    partial class DisplayScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            splitContainer1 = new SplitContainer();
+            btnLoad = new Button();
+            dataGridView1 = new DataGridView();
+            colName = new DataGridViewTextBoxColumn();
+            colSalary = new DataGridViewTextBoxColumn();
+            label2 = new Label();
+            label1 = new Label();
+            txtSalary = new TextBox();
+            txtName = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(btnLoad);
+            splitContainer1.Panel1.Controls.Add(dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Panel2.Controls.Add(txtSalary);
+            splitContainer1.Panel2.Controls.Add(txtName);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 257;
+            splitContainer1.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnLoad.Location = new Point(722, 231);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(75, 23);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { colName, colSalary });
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(800, 230);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
+            // 
+            // colName
+            // 
+            colName.HeaderText = "Name";
+            colName.Name = "colName";
+            colName.ReadOnly = true;
+            colName.Width = 200;
+            // 
+            // colSalary
+            // 
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = null;
+            colSalary.DefaultCellStyle = dataGridViewCellStyle1;
+            colSalary.HeaderText = "Salary";
+            colSalary.Name = "colSalary";
+            colSalary.ReadOnly = true;
+            colSalary.Width = 120;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(328, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 3;
+            label2.Text = "เงินเดือน";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(328, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 2;
+            label1.Text = "ชื่อ";
+            // 
+            // txtSalary
+            // 
+            txtSalary.Location = new Point(386, 98);
+            txtSalary.Name = "txtSalary";
+            txtSalary.Size = new Size(100, 23);
+            txtSalary.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(386, 51);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 0;
+            // 
+            // DisplayScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(splitContainer1);
+            Name = "DisplayScreen";
+            Size = new Size(800, 450);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private DataGridView dataGridView1;
+        private Button btnLoad;
+        private DataGridViewTextBoxColumn colName;
+        private DataGridViewTextBoxColumn colSalary;
+        private TextBox txtSalary;
+        private TextBox txtName;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ WinForm1/LoginScreen.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WinForm1
+{
+    partial class LoginScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtUserName = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(237, 169);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 15);
+            label1.TabIndex = 0;
+            label1.Text = "User Name";
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(472, 166);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(100, 23);
+            txtUserName.TabIndex = 1;
+            txtUserName.Text = "John";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(497, 220);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // LoginScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnLogin);
+            Controls.Add(txtUserName);
+            Controls.Add(label1);
+            Name = "LoginScreen";
+            Size = new Size(800, 450);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtUserName;
+        private Button btnLogin;
+    }
+}
